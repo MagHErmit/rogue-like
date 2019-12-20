8a8b09284fc2b2bdbf99606a58ab2964f5dea120
Vladislav <vladkakurin007@gmail.com>
2019-12-20 12:15:44 +1000

    add Hello World

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <ncurses.h>
+
+int main()
+{
+    initscr();                 // Инициализация ncurses
+    curs_set(0);               // Делаем курсор невидимым
+    printw("Hello, World!");   // Вывод строки Hello, World!
+    refresh();                 // Отображение строки на реальном экране
+    getch();                   // Ожидание пользовательского ввода
+    endwin();                  // Завершение работы с ncurses
+    return 0;
+}
